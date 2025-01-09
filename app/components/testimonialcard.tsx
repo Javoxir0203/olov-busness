96a00e66ec199da2626fac106e218f9084c0457b
Eshquvvatov Javoxir <javoxireshquvvatovv@gmail.com>
2025-01-09 17:20:52 +0500

    error

--- app/components/testimonialcard.tsx
+++ app/components/testimonialcard.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image"
-import logo from "@/public/Logo copy.svg"
+import zohid from '@/public/zohid-ulmasbaev.webp'
 interface TestimonialCardProps {
 	date: string
 	name: string
@@ -13,7 +13,7 @@ export function TestimonialCard({ date, name, position, content}: TestimonialCar
 		<div className='bg-teal-600 text-white rounded-lg overflow-hidden'>
 			<div className='grid md:grid-cols-2 gap-4'>
 				<div className='relative h-[300px] md:h-auto'>
-					<Image src={logo} alt={name} className='object-cover w-full h-full' width={100} height={100} />
+					<Image src={zohid} alt={name} className='object-cover w-full h-full' width={100} height={100} />
 				</div>
 				<div className='p-6 flex flex-col justify-between'>
 					<div>
